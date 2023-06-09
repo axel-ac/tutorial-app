60d22164e617df14e3bad372fccd1fec62d90465
axel-ac <abdullahceylan.axel@gmail.com>
2023-06-09 15:14:28 +0300

    func.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,30 @@
+import axios from "axios";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState([]);
+
+  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+
+  const getTutorials = async () => {
+    const { data } = await axios(BASE_URL);
+    setTutorials(data)
+    // console.log(data);
+  };
+
+  console.log(tutorials)
+
+  // didmount
+  useEffect(() => {
+    getTutorials();
+  }, []);
+
   return (
     <>
       <AddTutorial />
-      <TutorialList />
+      <TutorialList tutorials = {tutorials}/>
     </>
   );
 };
